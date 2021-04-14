0f3d54f5e737c3473a5cd4755788eb5c44edab3a
Birgit Plakso <birgit.plakso@gmail.com>
2021-04-14 16:29:27 +0300

    Klasside nimed muudetud

--- src/main/java/codewars/Ex9.java
+++ src/main/java/codewars/Ex9.java
@@ -1,12 +1,40 @@
 package codewars;
 
+import org.yaml.snakeyaml.util.ArrayUtils;
+
 import java.util.*;
 
 public class Ex9 {
 
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2, 1, 1, 1, 2, 3, 4, 3, 3, 4, 5, 4, 5, 6};
+        System.out.println(Arrays.toString(deleteNth(array, 2)));
+    }
+
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
 
-        //Code here ;)
-        return null;
+        int occurrence = 0;
+        ArrayList<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+
+                if (elements[i] == elements[j]) {
+                    occurrence = occurrence + 1;
+                }
+
+            }
+            if (occurrence <= maxOccurrences) {
+                intList.add(elements[i]);
+            }
+
+            occurrence=0;
+
+        }
+
+        int[] intArray = intList.stream().mapToInt(i -> i).toArray();
+
+
+        return intArray;
     }
 }
--- src/main/java/ee/bcs/valiit/controller/TestController.java
+++ src/main/java/ee/bcs/valiit/controller/TestController.java
@@ -1,20 +1,48 @@
 package ee.bcs.valiit.controller;
 
+import ee.bcs.valiit.sample.Employee;
 import ee.bcs.valiit.tasks.Lesson1;
 import ee.bcs.valiit.tasks.Lesson2;
 import ee.bcs.valiit.tasks.Lesson2b;
 import ee.bcs.valiit.tasks.Lesson3;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //meetodid või funktsioonid mis me tahame välja kutsuda: (restController käib *klassi* ette)
 @RestController
 public class TestController {
+
+    List<Employee> employeeList=new ArrayList<Employee>();
+
+    @GetMapping("employee/")
+    public List<Employee> dtoTest(){
+        return employeeList;
+    }
+
+    @GetMapping("employee/{id}")
+    public Employee dtoTest(@PathVariable("id") int index){
+        return employeeList.get(index);
+    }
+
+    @PostMapping("employee")
+    public void dtoAdd(@RequestBody Employee employee){
+        employeeList.add(employee);
+    }
+
+    @PutMapping("employee/{id}")
+    public void dtoReplace(@PathVariable("id") int index,@RequestBody Employee employee){
+        employeeList.set(index,employee);
+    }
+
+    @DeleteMapping("employee/{id}")
+    public void dtoDelete(@PathVariable("id") int index){
+        employeeList.remove(index);
+
+    }
 
     //GetMapping tuleb lisada meetodi ette
     @GetMapping("sample/hello-world/{nameInUrl}") //see aadress tuleb lisada veebi: localhost:8080/ sample/hello-world
